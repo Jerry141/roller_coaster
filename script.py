d7a8172c40d4d49695bcaf7162cb9fdbb8b4fc3f
Jerry <j.kociubinski93@gmail.com>
2021-09-03 15:56:18 +0200

    top 10 rankings function added

--- script.py
+++ script.py
@@ -26,6 +26,7 @@ def rank_year(name, park, data):
     ax.invert_yaxis()
     plt.show()
 
+
 # function that plots ranking comparison of a given roller coasters over time
 # name1 and name 2 - name of the roller coasters to compare
 # park1 and park 2 - name of the amusement parks where the roller coasters are located
@@ -51,6 +52,27 @@ def rank_years_comp(name1, name2, park1, park2, data):
     plt.show()
 
 
+# function that plots ranking comparison of roller coasters with rank <= n from data dataframe
+
+def rank_top_10(n, data):
+    top = data[data['Rank'] <= n]
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    for coaster in set(top['Name']):
+        coaster_rankings = top[top['Name'] == coaster].reset_index()
+        ax.plot(coaster_rankings['Year of Rank'], coaster_rankings['Rank'], label=coaster)
+        ax.set_yticks([i for i in range(1, 6)])
+
+        plt.title("Top 10 Rankings")
+        plt.xlabel("Year")
+        plt.ylabel("Rank")
+        plt.legend(loc=4)
+    ax.invert_yaxis()
+    plt.show()
+
+
 rank_year("El Toro", "Six Flags Great Adventure", wood_ranking)
 plt.clf()
 rank_years_comp("El Toro", "Boulder Dash", "Six Flags Great Adventure", "Lake Compounce", wood_ranking)
+plt.clf()
+rank_top_10(5, wood_ranking)
